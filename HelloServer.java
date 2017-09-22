4e701ef112fbb0af58b529be77173c8c4e513b48
cse360asu <cse360asu@gmail.com>
2017-09-22 08:25:08 -0700

    Sample java code

---
+++ HelloServer.java
@@ -0,0 +1,8 @@
+public class HelloServer {
+
+    public static void main(String[] args) {
+        // Prints "Hello, Server" to the terminal window.
+        System.out.println("Hello Server");
+    }
+
+}
---
+++ WorldServer.java
@@ -0,0 +1,8 @@
+public class WorldServer {
+
+    public static void main(String[] args) {
+        // Prints "World Server" to the terminal window.
+        System.out.println("World Server");
+    }
+
+}
